5f896e37b9bb794a3e7ca14a4e6f4439f66de08d
brahmdev <pandey.dev25@gmail.com>
2019-01-21 00:16:01 +0100

    unwanted component deleted and font awesome added

--- src/components/FooterBar.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an FooterBar page</h1>
-  </div>
-</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <Row class="header" type="flex" align="middle">
-      <div class="logo">
+      <!--<div class="logo">
         <img src="../assets/img/logo.png" height="30" alt="">
-        <span>VUE</span>MANAGER <Tag>beta</Tag>
-      </div>
+        <span>ACADEMY</span>-MANAGER <Tag>beta</Tag>
+      </div>-->
       <Dropdown class="login-info" placement="bottom-end">
         <Button type="ghost">
             <img src="../assets/img/photo.jpg" height="30" alt="">Admin
             <Icon type="arrow-down-b"></Icon>
         </Button> 
         <Dropdown-menu slot="list">
+            <Dropdown-item divided><i class="fa fa-cog"></i>Settings</Dropdown-item>
             <Dropdown-item divided><i class="fa fa-key"></i>Log out</Dropdown-item>
-            <Dropdown-item divided><i class="fa fa-cog"></i>Setting</Dropdown-item>
         </Dropdown-menu>
       </Dropdown>
     </Row>   
@@ -20,74 +20,183 @@
       <Menu theme="dark" width="100%" class="menu" :active-name="activeName" :accordion="true">
           <Menu-item name="Dashboard">
               <router-link to="/">
-                <i class="fa fa-dashboard"></i>
+                <img src="../assets/logo.png" height="30" alt="">
                 Dashboard  
               </router-link>
           </Menu-item>
-          <Menu-item name="Widget">
-              <router-link to="/widget">
-                <i class="fa fa-cogs"></i>
-                Widget  
-              </router-link>
-          </Menu-item>
-          <Menu-item name="Panels">
-              <router-link to="/panels">
-                <i class="fa fa-database"></i>
-                Panels
-              </router-link>
-          </Menu-item>
-          <Menu-item name="Editor">
-              <router-link to="/editor">
-                <i class="fa fa-pencil"></i>
-                Editor
-              </router-link>
-          </Menu-item>
-          <Menu-item name="ImageList">
-              <router-link to="/imagelist">
-                <i class="fa fa-photo"></i>
-                Image List  
-              </router-link>
-          </Menu-item>
-          <Menu-item name="Charts">
-              <router-link to="/charts">
-                <i class="fa fa-bar-chart"></i>
-                Charts  
-              </router-link>
-          </Menu-item>
-          <Submenu name="DataTable">
+          <Submenu name="Admission">
               <template slot="title">
-                  <i class="fa fa-database"></i>
-                  Data Table
+                  <i class="fas fa-plus"></i>
+                  Admission
               </template>
-              <Menu-item name="BasicTable">
+              <Menu-item name="Sub Task 1">
                 <router-link to="/basic-table">
-                  Basic Table 
+                  Sub Task 1 
                 </router-link>
               </Menu-item>
-              <Menu-item name="EditableTable">
+              <Menu-item name="Sub Task 2">
                 <router-link to="/editable-table">
-                  Editable Table  
+                  Sub Task 2 
                 </router-link>
               </Menu-item>
           </Submenu>
-          <Submenu name="Pages">
+          <Submenu name="Attendance">
               <template slot="title">
-                  <i class="fa fa-file"></i>
-                  Pages
+                  <i class="far fa-address-card"></i>
+                  Attendance
               </template>
-              <Menu-item name="Login">
-                <router-link to="/login">
-                  Login 
+              <Menu-item name="Sub Task 1">
+                <router-link to="/basic-table">
+                  Sub Task 1 
                 </router-link>
               </Menu-item>
-              <Menu-item name="Lockscreen">
-                <router-link to="/lockscreen">
-                  Lockscreen 
+              <Menu-item name="Sub Task 2">
+                <router-link to="/editable-table">
+                  Sub Task 2 
                 </router-link>
               </Menu-item>
-              <Menu-item name="Loading">
-                <router-link to="/loading">
-                  Loading
+          </Submenu>
+          <Submenu name="Fee">
+              <template slot="title">
+                  <i class="far fa-money-bill-alt"></i>
+                  Fee
+              </template>
+              <Menu-item name="Sub Task 1">
+                <router-link to="/basic-table">
+                  Sub Task 1 
+                </router-link>
+              </Menu-item>
+              <Menu-item name="Sub Task 2">
+                <router-link to="/editable-table">
+                  Sub Task 2 
+                </router-link>
+              </Menu-item>
+          </Submenu>
+          <Submenu name="Student">
+              <template slot="title">
+                  <i class="fas fa-user-graduate"></i>
+                  Student
+              </template>
+              <Menu-item name="Sub Task 1">
+                <router-link to="/basic-table">
+                  Sub Task 1 
+                </router-link>
+              </Menu-item>
+              <Menu-item name="Sub Task 2">
+                <router-link to="/editable-table">
+                  Sub Task 2 
+                </router-link>
+              </Menu-item>
+          </Submenu>
+          <Submenu name="Accounts">
+              <template slot="title">
+                  <i class="fas fa-calculator"></i>
+                  Accounts
+              </template>
+              <Menu-item name="Sub Task 1">
+                <router-link to="/basic-table">
+                  Sub Task 1 
+                </router-link>
+              </Menu-item>
+              <Menu-item name="Sub Task 2">
+                <router-link to="/editable-table">
+                  Sub Task 2 
+                </router-link>
+              </Menu-item>
+          </Submenu>
+          <Submenu name="Reports">
+              <template slot="title">
+                  <i class="fas fa-table"></i>
+                  Reports
+              </template>
+              <Menu-item name="Sub Task 1">
+                <router-link to="/basic-table">
+                  Sub Task 1 
+                </router-link>
+              </Menu-item>
+              <Menu-item name="Sub Task 2">
+                <router-link to="/editable-table">
+                  Sub Task 2 
+                </router-link>
+              </Menu-item>
+          </Submenu>
+          <Submenu name="Examination">
+              <template slot="title">
+                  <i class="fas fa-award"></i>
+                  Examination
+              </template>
+              <Menu-item name="Sub Task 1">
+                <router-link to="/basic-table">
+                  Sub Task 1 
+                </router-link>
+              </Menu-item>
+              <Menu-item name="Sub Task 2">
+                <router-link to="/editable-table">
+                  Sub Task 2 
+                </router-link>
+              </Menu-item>
+          </Submenu>
+          <Submenu name="Employees">
+              <template slot="title">
+                  <i class="fas fa-user-tie"></i>
+                  Employees
+              </template>
+              <Menu-item name="Sub Task 1">
+                <router-link to="/basic-table">
+                  Sub Task 1 
+                </router-link>
+              </Menu-item>
+              <Menu-item name="Sub Task 2">
+                <router-link to="/editable-table">
+                  Sub Task 2 
+                </router-link>
+              </Menu-item>
+          </Submenu>
+          <Submenu name="Classes">
+              <template slot="title">
+                  <i class="fas fa-school"></i>
+                  Classes
+              </template>
+              <Menu-item name="Sub Task 1">
+                <router-link to="/basic-table">
+                  Sub Task 1 
+                </router-link>
+              </Menu-item>
+              <Menu-item name="Sub Task 2">
+                <router-link to="/editable-table">
+                  Sub Task 2 
+                </router-link>
+              </Menu-item>
+          </Submenu>
+          <Submenu name="Seetings">
+              <template slot="title">
+                  <i class="fas fa-cogs"></i>
+                  Settings
+              </template>
+              <Menu-item name="Sub Task 1">
+                <router-link to="/basic-table">
+                  Sub Task 1 
+                </router-link>
+              </Menu-item>
+              <Menu-item name="Sub Task 2">
+                <router-link to="/editable-table">
+                  Sub Task 2 
+                </router-link>
+              </Menu-item>
+          </Submenu>
+          <Submenu name="Users">
+              <template slot="title">
+                  <i class="fas fa-users"></i>
+                  Users
+              </template>
+              <Menu-item name="Sub Task 1">
+                <router-link to="/basic-table">
+                  Sub Task 1 
+                </router-link>
+              </Menu-item>
+              <Menu-item name="Sub Task 2">
+                <router-link to="/editable-table">
+                  Sub Task 2 
                 </router-link>
               </Menu-item>
           </Submenu>
@@ -95,7 +204,7 @@
     </div>
     <div class="main-content">
       <router-view></router-view> 
-      <p class="vm-author">vue-manager designed by <a href="https://github.com/luosijie" target="_blank">Jesse Luo</a></p> 
+      <p class="vm-author">vue-manager maintained by <a href="https://github.com/brahmdev/" target="_blank">Brahmdev Pandey</a></p> 
     </div>
   </div>
   </div>
